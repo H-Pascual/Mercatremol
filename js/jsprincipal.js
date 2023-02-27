f27fbed0e5590b321835c320dadc9f3cc1afb146
MSI <hector.pascual.marin@gmail.com>
2023-02-27 12:40:23 +0100

    Volvemos a archivo anterior

---
+++ js/jsprincipal.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", (evento) => {
+
+let productos = [
+    {id: "1", nombre: "Huevos", precio: "1.99€", imagen: "Mercatremol/images/huevos.jpg"},
+    {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
+    {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
+];
+});
