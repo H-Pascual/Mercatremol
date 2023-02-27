94a9dd2dde278aeb3d5a7be870551bccbb4437ae
MSI <hector.pascual.marin@gmail.com>
2023-02-27 12:55:33 +0100

    Cambios añadidos

--- js/jsprincipal.js
+++ js/jsprincipal.js
@@ -1,8 +1,24 @@
 window.addEventListener("load", (evento) => {
 
-let productos = [
-    {id: "1", nombre: "Huevos", precio: "1.99€", imagen: "Mercatremol/images/huevos.jpg"},
-    {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
-    {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
-];
-});
+    let productos = [
+        {id: "1", nombre: "Huevos", precio: "1.99€", imagen: "Mercatremol/images/huevos.jpg"},
+        {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
+        {id: "3", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
+    ];
+    });
+    
+    /*
+    <li class="cart-box">
+                        <ul class="cart-list">
+                            <li>
+                                <a href="#" class="photo"><img src="images/img-pro-02.jpg" class="cart-thumb" alt="" /></a>
+                                <h6><a href="#">Tomate Cherry</a></h6>
+                                <p>1x - <span class="price">2.99 €</span></p>
+                            </li>
+                            <li class="total">
+                                <a href="#" class="btn btn-default hvr-hover btn-cart">Ver Carrito</a>
+                                <span class="float-right"><strong>Total</strong>: 2.99 €</span>
+                            </li>
+                        </ul>
+                    </li>
+    */
