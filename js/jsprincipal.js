7b733972fce3514005910e5f565c9d99342315c5
MSI <hector.pascual.marin@gmail.com>
2023-02-27 13:04:50 +0100

    Mejora texto

--- js/jsprincipal.js
+++ js/jsprincipal.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", (evento) => {
 
-    let productos = [
-        {id: "1", nombre: "Huevos", precio: "1.99€", imagen: "Mercatremol/images/huevos.jpg"},
-        {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
-        {id: "3", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
-    ];
-    });
-    
+let productos = [
+    {id: "1", nombre: "Huevos", precio: "1.99€", imagen: "Mercatremol/images/huevos.jpg"},
+    {id: "2", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
+    {id: "3", nombre: "Tomates", precio: "3.15€", imagen: "Mercatremol/images/tomate.jpg"},
+];
+});
+
     /*
     <li class="cart-box">
                         <ul class="cart-list">
